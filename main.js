0596a1b4872712545a084b5910731ab81dcd5a06
PotatoMath <82294627+PotatoMath@users.noreply.github.com>
2022-09-10 17:03:31 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+music_1 = "";
+music_2 = "";
+
+function preload()
+{
+    music_1 = loadSound("music.mp3");
+    music_2 = loadSound("music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    music_1.play();
+    music_2.play();
+}
